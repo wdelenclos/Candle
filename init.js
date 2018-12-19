ceac00bade6644f9907de794f0626d816005fb2b
WDelenclos <wdelenclos@gmail.com>
2018-12-19 17:15:43 +0100

    [Add] Router exemple

--- init.js
+++ init.js
@@ -3,14 +3,23 @@ import { Vrouter } from './core/vrouter/router.js';
 // configuration
 Vrouter.config({ mode: 'history'});
 
+// returning the user to the initial state
+Vrouter.navigate();
 
 // adding routes
-Vrouter.add(/home/, function() {
-        console.log('collection');
+Vrouter
+    .add(/dashboard/, function() {
+        console.log('⚓ Dashboard');
+    })
+    .add(/products\/(.*)\/edit\/(.*)/, function() {
+        console.log('products', arguments);
+    })
+    .add(function() {
+        Vrouter.navigate('/dashboard');
     });
 
 // forwarding
-Vrouter.navigate('/home');
+Vrouter.navigate('/dashboard');
 
 
 const $root = document.getElementById('root');
