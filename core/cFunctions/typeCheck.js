9511996523d41f59c0e07364cbc05de323429b73
jerome <jeromebueno55@gmail.com>
2019-02-05 10:48:31 +0100

    [FEAT] prop_access and typeCheck

--- core/cFunctions/typeCheck.js
+++ core/cFunctions/typeCheck.js
@@ -1,23 +1,43 @@
-
-
-export function typeCheck(data, conf) {
-    for (let key of Object.keys(conf)) {
+function type_check_v2(data, conf) {
+    for (var key of Object.keys(conf)) {
         switch (key) {
             case 'type':
+                if (!type_check_v1(data, conf[key])) return false;
+                break;
             case 'value':
+                if (JSON.stringify(data) !== JSON.stringify(conf[key])) return false;
+                break;
             case 'enum':
-                let newConf = {};
-                newConf[key] = conf[key];
-                if (!type_check_v2(data, newConf)) return false;
-                break;
-            case 'properties':
-                for (let prop of Object.keys(conf[key])) {
-                    if (data[prop] === undefined) return false;
-                    if (!type_check(data[prop], conf[key][prop])) return false;
+                var valid = false;
+                for (var value of conf[key]) {
+                    valid = type_check_v2(data, {value});
+                    if (valid) break;
                 }
-                break;
+                if(!valid) return false;
         }
     }
 
     return true;
 }
+
+export function typeCheck(data, conf) {
+        for (var key of Object.keys(conf)) {
+            switch (key) {
+                case 'type':
+                case 'value':
+                case 'enum':
+                    var newConf = {};
+                    newConf[key] = conf[key];
+                    if (!type_check_v2(data, newConf)) return false;
+                    break;
+                case 'properties':
+                    for (let prop of Object.keys(conf[key])) {
+                        if (data[prop] === undefined) return false;
+                        if (!type_check(data[prop], conf[key][prop])) return false;
+                    }
+                    break;
+            }
+        }
+        return true;
+}
+
