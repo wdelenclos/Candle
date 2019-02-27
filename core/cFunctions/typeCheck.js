10c58ba2bcc57fe7c08ad1e9beab1e86d344c148
jerome <jeromebueno55@gmail.com>
2019-02-27 17:02:59 +0100

    [ADD] primitive type check

--- core/cFunctions/typeCheck.js
+++ core/cFunctions/typeCheck.js
@@ -1,8 +1,30 @@
-function type_check_v2(data, conf) {
+function primitiveTypeCheck(data, type) {
+    switch(typeof data) {
+        case "number":
+        case "string":
+        case "boolean":
+        case "undefined":
+        case "function":
+            return type === typeof data;
+        case "object":
+            switch(type) {
+                case "null":
+                    return data === null;
+                case "array":
+                    return Array.isArray(data);
+                default:
+                    return data !== null && !Array.isArray(data);
+            }
+
+    }
+    return false;
+}
+
+function complexTypeCheck(data, conf) {
     for (var key of Object.keys(conf)) {
         switch (key) {
             case 'type':
-                if (!type_check_v1(data, conf[key])) return false;
+                if (!primitiveTypeCheck(data, conf[key])) return false;
                 break;
             case 'value':
                 if (JSON.stringify(data) !== JSON.stringify(conf[key])) return false;
@@ -10,7 +32,7 @@ function type_check_v2(data, conf) {
             case 'enum':
                 var valid = false;
                 for (var value of conf[key]) {
-                    valid = type_check_v2(data, {value});
+                    valid = complexTypeCheck(data, {value});
                     if (valid) break;
                 }
                 if(!valid) return false;
@@ -28,12 +50,12 @@ export function typeCheck(data, conf) {
                 case 'enum':
                     var newConf = {};
                     newConf[key] = conf[key];
-                    if (!type_check_v2(data, newConf)) return false;
+                    if (!complexTypeCheck(data, newConf)) return false;
                     break;
                 case 'properties':
                     for (let prop of Object.keys(conf[key])) {
                         if (data[prop] === undefined) return false;
-                        if (!type_check(data[prop], conf[key][prop])) return false;
+                        if (!typeCheck(data[prop], conf[key][prop])) return false;
                     }
                     break;
             }
